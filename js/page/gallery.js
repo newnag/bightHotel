d86ebe293b21ff0afd77f0dbf8aa1b1b81b943e3
newnag <48855584+newnag@users.noreply.github.com>
2020-07-02 17:35:08 +0700

    02/07/2020

--- js/page/gallery.js
+++ js/page/gallery.js
@@ -9,7 +9,11 @@ function openBigImg(){
     const img = document.querySelectorAll('.gallary-zone figure img')
     img.forEach(Img=>{
         Img.addEventListener('click',()=>{
-            document.querySelector('.showpic').
+            document.querySelector('.showpic').classList.add('active')
         })
     })
+}
+
+function closeBigImg(){
+    const close = document.querySelectorAll('.bigpic .close')
 }
